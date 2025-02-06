2d774c87ac7f13cf2e02c3130c6487adc7e89fb8
Daewon Kang <alexnori.com@gmail.com>
2025-02-06 15:42:32 +0900

    feat: 회원가입 API 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,23 +16,30 @@ import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { AuthGuard } from './guards/auth.guard';
+import { PassportLocalGuard } from './guards/passport-local.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('signUp')
+  createUser(@Body() createAuthDto: CreateAuthDto) {
+    return this.authService.createUser(createAuthDto);
+  }
+
   @HttpCode(HttpStatus.OK)
   @Post('login')
+  @UseGuards(PassportLocalGuard)
   login(@Body() input: { username: string; password: string }) {
     return this.authService.authenticate(input);
   }
 
-  @UseGuards(AuthGuard)
-  @Get('me')
-  getUserInfo(@Request() request) {
-    if (!request.user) {
-      throw new NotImplementedException();
-    }
-    return request.user;
-  }
+  // @UseGuards(AuthGuard)
+  // @Get('me')
+  // getUserInfo(@Request() request) {
+  //   if (!request.user) {
+  //     throw new NotImplementedException();
+  //   }
+  //   return request.user;
+  // }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,17 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 interface AuthInput {
-  username: string;
+  email: string;
   password: string;
 }
 interface SignInData {
@@ -13,10 +19,17 @@ interface SignInData {
   username: string;
 }
 
+interface User {
+  id: string;
+  email: string;
+  nickname: string;
+  image?: string;
+}
+
 export interface AuthResult {
   accessToken: string;
-  userId: number;
-  username: string;
+  refreshToken: string;
+  user: User;
 }
 
 @Injectable()
@@ -24,7 +37,68 @@ export class AuthService {
   constructor(
     private userService: UsersService,
     private jwtService: JwtService,
+    private prisma: PrismaService,
   ) {}
+
+  async createUser(createAuthDto: CreateAuthDto) {
+    // 이메일 중복이 없는지 확인
+    const emailExists = await this.prisma.user.findUnique({
+      where: { email: createAuthDto.email }, // 이메일이 이미 존재하는지 확인
+    });
+    if (emailExists) {
+      throw new ConflictException('Email already exists');
+    }
+
+    // 닉네임 중복이 없는지 확인
+    const nicknameExists = await this.prisma.user.findUnique({
+      where: { nickname: createAuthDto.nickname }, // 닉네임이 이미 존재하는지 확인
+    });
+    if (nicknameExists) {
+      throw new ConflictException('Nickname already exists');
+    }
+
+    // 비밀번호 암호화
+    const saltRounds = 10;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const hashedPassword = (await bcrypt.hash(
+      createAuthDto.password,
+      saltRounds,
+    )) as string;
+
+    // 사용자 생성
+    const user = await this.prisma.user.create({
+      data: {
+        email: createAuthDto.email,
+        nickname: createAuthDto.nickname,
+        password: hashedPassword,
+      },
+    });
+
+    // accessToken, refreshToken 생성
+    const tokenPayload = {
+      sub: user.id,
+      email: user.email,
+      nickname: user.nickname,
+    };
+    const accessToken = await this.jwtService.signAsync(tokenPayload, {
+      expiresIn: '1h',
+    });
+    const refreshToken = await this.jwtService.signAsync(tokenPayload, {
+      expiresIn: '7d',
+    });
+
+    // 사용자 정보 반환
+    return {
+      accessToken,
+      refreshToken,
+      user: {
+        id: user.id,
+        email: user.email,
+        nickname: user.nickname,
+        image: null,
+      },
+    };
+  }
 
   async authenticate(input: AuthInput): Promise<AuthResult> {
     const user = await this.validateUser(input);
