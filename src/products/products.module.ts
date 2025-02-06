95ecaf1a78381efd47e0c95d68232b87179222e3
Daewon Kang <alexnori.com@gmail.com>
2025-02-06 11:38:44 +0900

    feat: 이미지 업로드 모듈 추가

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { ImageModule } from './image/image.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
+  imports: [ImageModule],
 })
 export class ProductsModule {}
