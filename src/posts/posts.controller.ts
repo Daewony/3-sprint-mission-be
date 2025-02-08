f4cafe0193bfa5f142d2441771ae647c7d13cb68
Daewon Kang <alexnori.com@gmail.com>
2025-02-08 23:15:39 +0900

    feat: 게시글 등록 API 구현

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,34 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { PostsService } from './posts.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Request,
+  UseGuards,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
+import { PassportJwtAuthGuard } from 'src/auth/guards/passport-jwt.guard';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postsService.create(createPostDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.postsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postsService.update(+id, updatePostDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postsService.remove(+id);
+  @UseGuards(PassportJwtAuthGuard)
+  @ApiOperation({ summary: '게시글 등록' })
+  createPost(
+    @Body() createPostDto: CreatePostDto,
+    @Request() request: { user?: { userId: string } },
+  ) {
+    if (!request.user) throw new UnauthorizedException('로그인이 필요합니다.');
+    const ownerId = request.user.userId;
+    return this.postsService.createPost(createPostDto, ownerId);
   }
 }
